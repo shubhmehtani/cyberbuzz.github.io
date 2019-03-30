5a0b6099a2187d3612e105416b9e237bdfd011d3
CodeSarthak <getsarthakaggarwal@gmail.com>
2019-03-30 21:08:13 +0530

    Initial Commits

---
+++ script.js
@@ -0,0 +1,110 @@
+window.onload = function(){
+
+
+document.getElementById("hv-1").onclick = function() {
+    document.getElementById('myModal-01').style.display = "block";
+}
+
+document.getElementById("hv-2").onclick = function() {
+    document.getElementById('myModal-02').style.display = "block";
+}
+
+document.getElementById("hv-3").onclick = function() {
+   document.getElementById('myModal-03').style.display = "block";
+}
+
+document.getElementById("hv-4").onclick = function() {
+    document.getElementById('myModal-04').style.display = "block";
+}
+
+document.getElementById("hv-5").onclick = function() {
+    document.getElementById('myModal-05').style.display = "block";
+}
+
+document.getElementById("hv-6").onclick = function() {
+    document.getElementById('myModal-06').style.display = "block";
+}
+
+document.getElementById("hv-7").onclick = function() {
+    document.getElementById('myModal-07').style.display = "block";
+}
+
+document.getElementById("hv-8").onclick = function() {
+    document.getElementById('myModal-08').style.display = "block";
+}
+
+document.getElementById("hv-9").onclick = function() {
+    document.getElementById('myModal-09').style.display = "block";
+}
+
+document.getElementById("hv-10").onclick = function() {
+    document.getElementById('myModal-10').style.display = "block";
+}
+
+document.getElementById("hv-11").onclick = function() {
+    document.getElementById('myModal-11').style.display = "block";
+}
+
+document.getElementById("hv-13").onclick = function() {
+    document.getElementById('myModal-13').style.display = "block";
+}
+
+document.getElementById("hv-14").onclick = function() {
+    document.getElementById('myModal-14').style.display = "block";
+}
+
+document.getElementById("hv-15").onclick = function() {
+    document.getElementById('myModal-15').style.display = "block";
+}
+
+
+
+
+
+
+window.onclick = function(event) {
+    if (event.target == document.getElementById('myModal-01')) {
+       document.getElementById('myModal-01').style.display = "none";
+    }
+    else if (event.target == document.getElementById('myModal-02')) {
+       document.getElementById('myModal-02').style.display = "none";
+    }
+    else if (event.target == document.getElementById('myModal-03')) {
+       document.getElementById('myModal-03').style.display = "none";
+    }
+    else if (event.target == document.getElementById('myModal-04')) {
+       document.getElementById('myModal-04').style.display = "none";
+    }
+    else if (event.target == document.getElementById('myModal-05')) {
+       document.getElementById('myModal-05').style.display = "none";
+    }
+    else if (event.target == document.getElementById('myModal-06')) {
+        document.getElementById('myModal-06').style.display = "none";
+    }
+    else if (event.target == document.getElementById('myModal-07')) {
+       document.getElementById('myModal-07').style.display = "none";
+    }
+    else if (event.target == document.getElementById('myModal-08')) {
+       document.getElementById('myModal-08').style.display = "none";
+    }
+    else if (event.target == document.getElementById('myModal-09')) {
+       document.getElementById('myModal-09').style.display = "none";
+    }
+    else if (event.target == document.getElementById('myModal-10')) {
+       document.getElementById('myModal-10').style.display = "none";
+    }
+    else if (event.target == document.getElementById('myModal-11')) {
+       document.getElementById('myModal-11').style.display = "none";
+    }
+    else if (event.target == document.getElementById('myModal-13')) {
+       document.getElementById('myModal-13').style.display = "none";
+    }
+    else if (event.target == document.getElementById('myModal-14')) {
+       document.getElementById('myModal-14').style.display = "none";
+    }
+    else if (event.target == document.getElementById('myModal-15')) {
+       document.getElementById('myModal-15').style.display = "none";
+    }
+}
+
+}
